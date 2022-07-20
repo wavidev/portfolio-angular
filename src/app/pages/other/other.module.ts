f6221cd2fa7841579d75c379de0d278507ad8938
wavidev <wanthervince@gmail.com>
2022-07-20 16:08:28 -0500

    creando componentes y pages

---
+++ src/app/pages/other/other.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OtherComponent } from './other.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [OtherComponent]
+})
+export class OtherModule { }
